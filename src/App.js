d202543112cb43345c5c121cc4ce47c47bb95b74
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-20 14:14:19 +0100

    converting the value to a number

--- src/App.js
+++ src/App.js
@@ -30,7 +30,10 @@ function Form() {
   return (
     <form className="add-form" onSubmit={handlesubmit}>
       <h3>What do you need for your trip 😻 </h3>
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
