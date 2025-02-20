d573433af9bd0b00ed5b183cd2a3612d06e9ba8c
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-20 13:50:00 +0100

    creating a state for the description

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,6 +22,7 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState();
   function handlesubmit(e) {
     e.preventDefault();
   }
