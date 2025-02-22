f0c6ef3e6c7013a3047582daafb83e789ea9c781
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-22 23:13:09 +0100

    creating a file for the Logo and importing it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Logo from "./Logo";
 
 export default function App() {
   const [item, setItem] = useState([]);
@@ -39,10 +40,6 @@ export default function App() {
       <Stats items={item} />
     </div>
   );
-}
-
-function Logo() {
-  return <h1>🌴 Far Away 💼</h1>;
 }
 
 function Form({ onAddItems }) {
