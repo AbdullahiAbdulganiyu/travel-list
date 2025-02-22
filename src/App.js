bfb2b9d9767652e8d0c7861d3e17a4daf54a416d
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-22 11:17:34 +0100

    adding a checkbox

--- src/App.js
+++ src/App.js
@@ -81,6 +81,7 @@ function PackingList({ items, onDeleteItem }) {
 function Item({ item, onDeleteItem }) {
   return (
     <li>
+      <input type="checkbox" />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.description} {item.quantity}
       </span>
